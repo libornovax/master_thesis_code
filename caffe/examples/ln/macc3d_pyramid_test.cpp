069de6b7f13cc1c66def02ab1c271f580b15cbf9
Libor Novak <libor.new@gmail.com>
2017-03-21 15:26:08 +0100

    Split macc tester to two different executables - one for BBTXT and one for BB3TXT

--- caffe/examples/ln/macc3d_pyramid_test.cpp
+++ caffe/examples/ln/macc3d_pyramid_test.cpp
@@ -1,8 +1,8 @@
 //
 // Libor Novak
-// 03/20/2017
+// 03/21/2017
 //
-// Tests object detection of 2D bounding boxes a single scale detector on an image pyramid. It outputs BBTXT.
+// Tests object detection of 3D bounding boxes of a single scale detector on an image pyramid
 //
 // Optionaly it can apply non-maxima suppression (merging) on bounding boxes.
 //
@@ -24,34 +24,6 @@
 
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
-
-
-namespace {
-
-    struct BB2D
-    {
-        BB2D () {}
-        BB2D (const std::string &path_image, int label, double conf, double xmin, double ymin, double xmax,
-              double ymax)
-            : path_image(path_image),
-              label(label),
-              conf(conf),
-              xmin(xmin),
-              ymin(ymin),
-              xmax(xmax),
-              ymax(ymax)
-        {}
-
-        std::string path_image;
-        int label;
-        double conf;
-        double xmin;
-        double ymin;
-        double xmax;
-        double ymax;
-    };
-
-}
 
 
 /**
@@ -77,21 +49,22 @@ void wrapInputLayer (caffe::Blob<float>* input_layer, std::vector<cv::Mat> &out_
 }
 
 
-std::vector<BB2D> extract2DBoundingBoxes (caffe::Blob<float> *output, const std::string &path_image,
-                                          double scale)
+void extract3DBoundingBoxes (caffe::Blob<float> *output, const std::string &path_image, double scale,
+                             std::ofstream &fout)
 {
-    std::vector<BB2D> bounding_boxes;
-
     float *data_output = output->mutable_cpu_data();
 
-    // 2D bounding box
+    // 3D bounding box
     cv::Mat acc_prob(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 0));
-    cv::Mat acc_xmin(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 1));
-    cv::Mat acc_ymin(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 2));
-    cv::Mat acc_xmax(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 3));
-    cv::Mat acc_ymax(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 4));
+    cv::Mat acc_fblx(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 1));
+    cv::Mat acc_fbly(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 2));
+    cv::Mat acc_fbrx(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 3));
+    cv::Mat acc_fbry(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 4));
+    cv::Mat acc_rblx(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 5));
+    cv::Mat acc_rbly(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 6));
+    cv::Mat acc_ftly(output->shape(2), output->shape(3), CV_32FC1, data_output+output->offset(0, 7));
 
-    // Extract detected boxes
+    // Extract detected 3D boxes
     for (int i = 0; i < acc_prob.rows; ++i)
     {
         for (int j = 0; j < acc_prob.cols; ++j)
@@ -99,68 +72,20 @@ std::vector<BB2D> extract2DBoundingBoxes (caffe::Blob<float> *output, const std:
             float conf = acc_prob.at<float>(i, j);
             if (conf >= 0.1)
             {
-                int xmin = (4*j + (80*(acc_xmin.at<float>(i, j) - 0.5))) / scale;
-                int ymin = (4*i + (80*(acc_ymin.at<float>(i, j) - 0.5))) / scale;
-                int xmax = (4*j + (80*(acc_xmax.at<float>(i, j) - 0.5))) / scale;
-                int ymax = (4*i + (80*(acc_ymax.at<float>(i, j) - 0.5))) / scale;
+                int fblx = (4*j + (80*(acc_fblx.at<float>(i, j) - 0.5))) / scale;
+                int fbly = (4*i + (80*(acc_fbly.at<float>(i, j) - 0.5))) / scale;
+                int fbrx = (4*j + (80*(acc_fbrx.at<float>(i, j) - 0.5))) / scale;
+                int fbry = (4*i + (80*(acc_fbry.at<float>(i, j) - 0.5))) / scale;
+                int rblx = (4*j + (80*(acc_rblx.at<float>(i, j) - 0.5))) / scale;
+                int rbly = (4*i + (80*(acc_rbly.at<float>(i, j) - 0.5))) / scale;
+                int ftly = (4*i + (80*(acc_ftly.at<float>(i, j) - 0.5))) / scale;
 
-                bounding_boxes.emplace_back(path_image, 1, conf, xmin, ymin, xmax, ymax);
+                // The line of a BB3TXT file is:
+                // filename label confidence xmin ymin xmax ymax fblx fbly fbrx fbry rblx rbly ftly
+                fout << path_image << " 1 " << conf << " 0 0 0 0 " << fblx << " " << fbly << " " << fbrx
+                     << " " << fbry << " " << rblx << " " << rbly << " " << ftly << std::endl;
             }
         }
-    }
-
-    return bounding_boxes;
-}
-
-
-std::vector<BB2D> detectObjects (const std::string &path_image, const cv::Mat &image,
-                                 const std::vector<double> &scales,
-                                 const std::shared_ptr<caffe::Net<float>> &net)
-{
-    std::vector<BB2D> bounding_boxes;
-
-    caffe::Blob<float>* input_layer  = net->input_blobs()[0];
-    caffe::Blob<float>* output_layer = net->output_blobs()[0];
-
-    std::vector<cv::Mat> input_channels;
-
-    // Convert to zero mean and unit variance
-    cv::Mat imagef; image.convertTo(imagef, CV_32FC3);
-    imagef -= cv::Scalar(128.0f, 128.0f, 128.0f);
-    imagef *= 1.0f/128.0f;
-
-    // Build the image pyramid and run detection on each scale of the pyramid
-    for (double s: scales)
-    {
-        cv::Mat imagef_scaled;
-        cv::resize(imagef, imagef_scaled, cv::Size(), s, s);
-
-        // Reshape the network
-        input_layer->Reshape(1, input_layer->shape(1), imagef_scaled.rows, imagef_scaled.cols);
-        net->Reshape();
-
-        // Prepare the cv::Mats for input
-        wrapInputLayer(input_layer, input_channels);
-        // Copy the image to the input layer of the network
-        cv::split(imagef_scaled, input_channels);
-
-        net->Forward();
-
-        std::vector<BB2D> new_bbs = extract2DBoundingBoxes(output_layer, path_image, s);
-        bounding_boxes.insert(bounding_boxes.end(), new_bbs.begin(), new_bbs.end());
-    }
-
-    return bounding_boxes;
-}
-
-
-void writeBoundingBoxes (const std::vector<BB2D> &bbs, std::ofstream &fout)
-{
-    for (const BB2D &bb: bbs)
-    {
-        // The line of a BBTXT file is: filename label confidence xmin ymin xmax ymax
-        fout << bb.path_image << " " << bb.label << " " << bb.conf << " " << bb.xmin << " " << bb.ymin
-             << " " << bb.xmax << " " << bb.ymax << std::endl;
     }
 }
 
@@ -188,7 +113,11 @@ void runPyramidDetection (const std::string &path_prototxt, const std::string &p
 
     CHECK_EQ(net->num_inputs(), 1) << "Network should have exactly one input.";
     CHECK_EQ(input_layer->shape(1), 3) << "Input layer must have 3 channels.";
-    CHECK_EQ(output_layer->shape(1), 5) << "Unsupported network, only 5 channels!";
+    CHECK_EQ(output_layer->shape(1), 8) << "Unsupported network, only 8 output channels supported!";
+
+    // Prepare the input channels
+    std::vector<cv::Mat> input_channels;
+    wrapInputLayer(input_layer, input_channels);
 
     std::ifstream infile(path_image_list.c_str());
     CHECK(infile) << "Unable to open image list TXT file '" << path_image_list << "'!";
@@ -206,12 +135,34 @@ void runPyramidDetection (const std::string &path_prototxt, const std::string &p
 
         // Load the image
         cv::Mat image = cv::imread(line, CV_LOAD_IMAGE_COLOR);
+        cv::Mat imagef; image.convertTo(imagef, CV_32FC3);
+
+        // Convert to zero mean and unit variance
+        imagef -= cv::Scalar(128.0f, 128.0f, 128.0f);
+        imagef *= 1.0f/128.0f;
 
         // Build the image pyramid and run detection on each scale of the pyramid
-        std::vector<BB2D> bbs = detectObjects(line, image, scales, net);
+        for (double s: scales)
+        {
+            cv::Mat imagef_scaled; cv::resize(imagef, imagef_scaled, cv::Size(), s, s);
 
-        // Save the bounding boxes to a BBTXT file
-        writeBoundingBoxes(bbs, fout);
+            if (imagef_scaled.rows != input_layer->shape(2) || imagef_scaled.cols != input_layer->shape(3))
+            {
+                // Reshape the network
+                input_layer->Reshape(1, input_layer->shape(1), imagef_scaled.rows, imagef_scaled.cols);
+                net->Reshape();
+
+                wrapInputLayer(input_layer, input_channels);
+            }
+
+            // Copy the image to the input layer of the network
+            cv::split(imagef_scaled, input_channels);
+
+            net->Forward();
+
+            // 3D bounding box
+            extract3DBoundingBoxes(output_layer, line, s, fout);
+        }
     }
 
     fout.close();
@@ -246,7 +197,7 @@ void parseArguments (int argc, char** argv, ProgramArguments &pa)
             ("image_list", po::value<std::string>(&pa.path_image_list)->required(),
              "Path to a TXT file with paths to the images to be tested")
             ("path_out", po::value<std::string>(&pa.path_out)->required(),
-             "Path to the output BBTXT file")
+             "Path to the output BB3TXT file")
         ;
 
         po::positional_options_description positional;
@@ -261,7 +212,7 @@ void parseArguments (int argc, char** argv, ProgramArguments &pa)
         po::store(po::command_line_parser(argc, argv).options(desc).positional(positional).run(), vm);
 
         if (vm.count("help")) {
-            std::cout << "Usage: ./detect_accumulator path/f.prototxt path/f.caffemodel path/image_list.txt path/out.bbtxt\n";
+            std::cout << "Usage: ./detect_accumulator path/f.prototxt path/f.caffemodel path/image_list.txt path/out.bb3txt\n";
             std::cout << desc;
             exit(EXIT_SUCCESS);
         }
